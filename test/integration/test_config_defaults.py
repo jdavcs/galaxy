cacdbca55f84ef86a974a1b74c64c50f01b8fb37
Sergey Golitsynskiy <sgolitsynskiy@gmail.com>
2019-10-17 00:23:37 -0400

    WIP (MOST test run)

--- test/integration/test_config_defaults.py
+++ test/integration/test_config_defaults.py
@@ -1,6 +1,10 @@
+import os
 import pytest
 from base.driver_util import GalaxyTestDriver
 from collections import namedtuple
+
+
+OptionData = namedtuple('OptionData', 'expected, loaded')
 
 
 @pytest.fixture(scope='module')
@@ -17,22 +21,39 @@ def create_driver():
 
 def get_config_data():
     create_driver()
+    parent_dirs = load_parent_dirs()
 
     for key, data in DRIVER.app.config.appschema.items():
-        expected_value = get_expected(key, data)
+        expected_value = get_expected(key, data, parent_dirs)
         loaded_value = getattr(DRIVER.app.config, key)
-        data = (expected_value, loaded_value)
+
+        data = OptionData(expected=expected_value, loaded=loaded_value)
+
+        #data = (expected_value, loaded_value)
         yield pytest.param(data)
 
 
+def load_parent_dirs():
+    parent_dirs = {
+        'root_dir': DRIVER.app.config.root,
+        'config_dir': DRIVER.app.config.config_dir,
+        'data_dir': DRIVER.app.config.data_dir,
+        'tool_data_path': DRIVER.app.config.tool_data_path,
+    }
+    return parent_dirs
 
-def get_expected(key, data):
-    return data.get('default')  #fix this
 
+def get_expected(key, data, parent_dirs):
+    value = data.get('default')
 
+    parent = data.get('path_resolves_to')
+    if parent:
+        value = parent_dirs[parent]
+
+    return value
 
 
 @pytest.mark.parametrize('data', get_config_data())
 def test_config_option(data, driver):
     print(data)
-    assert True
+    assert data.expected == data.loaded
