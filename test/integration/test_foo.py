eb09c427596f9711830b5c2eacbda0a9e95aef11
Sergey Golitsynskiy <sgolitsynskiy@gmail.com>
2020-04-15 14:31:13 -0400

    WIP

--- test/integration/test_foo.py
+++ test/integration/test_foo.py
@@ -1,6 +1,13 @@
+import os
+from collections import namedtuple
+from datetime import timedelta
+
 import pytest
 
-from galaxy_test.driver.driver_util import GalaxyConfigTestDriver as Driver
+from galaxy.util import listify
+from galaxy_test.driver.driver_util import GalaxyConfigTestDriver
+
+OptionData = namedtuple('OptionData', ('key', 'expected', 'loaded'))
 
 
 @pytest.fixture(scope='module')
@@ -11,16 +18,21 @@ def driver(request):
 
 def create_driver():
     global DRIVER
-    DRIVER = Driver()
+    DRIVER = GalaxyConfigTestDriver()
     DRIVER.setup()
 
 
 def get_config_data():
-    create_driver()
-    config = DRIVER.app.config
-    return [config.schema.app_schema['admin_users']]
+    create_driver()  # create + setup DRIVER
+    items = ((k, v) for k, v in DRIVER.app.config.schema.app_schema.items())
+    for key, data in items:
+        data = OptionData(key=key, expected='42', loaded='42')
+        yield pytest.param(data)
 
 
-@pytest.mark.parametrize('data', get_config_data())
-def test_one(data, driver):
-    assert True
+def get_key(option_data):
+    return option_data.key
+
+@pytest.mark.parametrize('data', get_config_data(), ids=get_key)
+def test_config_option(data, driver):
+    assert data.expected == data.loaded
