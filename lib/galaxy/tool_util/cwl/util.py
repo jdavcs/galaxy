775bbe5eef8b5a86110faa925e911a95f6a49ee9
Simon Bray <sbray1371@gmail.com>
2020-09-30 10:47:58 +0200

    remove copy_func, appears unnecessary

--- lib/galaxy/tool_util/cwl/util.py
+++ lib/galaxy/tool_util/cwl/util.py
@@ -84,7 +84,7 @@ def path_or_uri_to_uri(path_or_uri):
 
 
 def galactic_job_json(
-    job, test_data_directory, upload_func, copy_func, collection_create_func, tool_or_workflow="workflow"
+    job, test_data_directory, upload_func, collection_create_func, tool_or_workflow="workflow"
 ):
     """Adapt a CWL job object to the Galaxy API.
 
@@ -145,7 +145,7 @@ def galactic_job_json(
         is_file = item_class == "File"
         is_directory = item_class == "Directory"
         is_collection = item_class == "Collection"  # Galaxy extension.
-        is_galaxy_id = item_class == "GalaxyID"  # Galaxy dataset/collection ID.
+        # is_galaxy_id = item_class == "GalaxyID"  # Galaxy dataset/collection ID.
 
         if force_to_file:
             if is_file:
@@ -168,8 +168,8 @@ def galactic_job_json(
             return replacement_directory(value)
         elif is_collection:
             return replacement_collection(value)
-        elif is_galaxy_id:
-            return replacement_galaxy_id(value)
+        # elif is_galaxy_id:
+        #     return replacement_galaxy_id(value)
         else:
             return replacement_record(value)
 
@@ -295,10 +295,7 @@ def galactic_job_json(
         return {"src": "hdca", "id": hdca_id}
 
     def replacement_galaxy_id(value):
-        response = copy_func(value['location'])
-        target = FileLiteralTarget(contents=None)
-        datasets.append((response, target))
-        return {"src": "hda", "id": value['location']}
+        return {"src": "hda", "id": value['galaxy_id']}
 
     def replacement_record(value):
         collection_element_identifiers = []
