e470757c29c0010700900daf1e86ed400014e130
Sergey Golitsynskiy <sgolitsynskiy@gmail.com>
2021-12-01 17:22:07 -0500

    Update config, mapping.py to use migrations process

--- lib/galaxy/model/mapping.py
+++ lib/galaxy/model/mapping.py
@@ -11,10 +11,7 @@ from typing import Optional, Type
 from galaxy import model
 from galaxy.model import mapper_registry
 from galaxy.model.base import SharedModelMapping
-from galaxy.model.migrate.triggers.update_audit_table import install as install_timestamp_triggers
-from galaxy.model.orm.engine_factory import build_engine
 from galaxy.model.security import GalaxyRBACAgent
-from galaxy.model.view.utils import install_views
 
 log = logging.getLogger(__name__)
 
@@ -29,38 +26,24 @@ class GalaxyModelMapping(SharedModelMapping):
     GalaxySession: Type
 
 
-def init(file_path, url, engine_options=None, create_tables=False, map_install_models=False,
-        database_query_profiling_proxy=False, object_store=None, trace_logger=None, use_pbkdf2=True,
-        slow_query_log_threshold=0, thread_local_log: Optional[local] = None, log_query_counts=False) -> GalaxyModelMapping:
-    """Connect mappings to the database"""
-    if engine_options is None:
-        engine_options = {}
-    # Connect dataset to the file path
+def init(
+    file_path,
+    engine,
+    map_install_models=False,
+    object_store=None,
+    use_pbkdf2=True,
+    thread_local_log: Optional[local] = None
+) -> GalaxyModelMapping:
     model.Dataset.file_path = file_path
-    # Connect dataset to object store
     model.Dataset.object_store = object_store
-    # Use PBKDF2 password hashing?
     model.User.use_pbkdf2 = use_pbkdf2
-    # Load the appropriate db module
-    engine = build_engine(url, engine_options, database_query_profiling_proxy, trace_logger, slow_query_log_threshold, thread_local_log=thread_local_log, log_query_counts=log_query_counts)
 
     model_modules = [model]
     if map_install_models:
-        import galaxy.model.tool_shed_install.mapping  # noqa: F401
         from galaxy.model import tool_shed_install
-        galaxy.model.tool_shed_install.mapping.init(url=url, engine_options=engine_options, create_tables=create_tables)
         model_modules.append(tool_shed_install)
 
-    result = GalaxyModelMapping(model_modules, engine=engine)
-
-    # Create tables if needed
-    if create_tables:
-        metadata.create_all(bind=engine)
-        install_timestamp_triggers(engine)
-        install_views(engine)
-
-    result.create_tables = create_tables
-    # load local galaxy security policy
-    result.security_agent = GalaxyRBACAgent(result)
-    result.thread_local_log = thread_local_log
-    return result
+    model_mapping = GalaxyModelMapping(model_modules, engine=engine)
+    model_mapping.security_agent = GalaxyRBACAgent(model_mapping)
+    model_mapping.thread_local_log = thread_local_log
+    return model_mapping
