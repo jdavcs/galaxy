d9484386fa0efa1a85dc929534be51c8e675c43a
Sergey Golitsynskiy <sgolitsynskiy@gmail.com>
2021-12-27 15:09:56 -0500

    Setup python script to use alembic

--- scripts/manage_db.py
+++ scripts/manage_db.py
@@ -1,28 +1,31 @@
-""" This script parses Galaxy or Tool Shed config file for database connection
-and then delegates to sqlalchemy_migrate shell main function in
-migrate.versioning.shell. """
+""" 
+TODO: add description
+"""
 import logging
 import os.path
 import sys
 
-from migrate.versioning.shell import main
+import alembic.config
 
 sys.path.insert(1, os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'lib')))
 
+from galaxy.config import GalaxyAppConfiguration
 from galaxy.model.orm.scripts import get_config
 
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
 
-def invoke_migrate_main():
-    # Migrate has its own args, so cannot use argparse
-    config = get_config(sys.argv, use_argparse=False, cwd=os.getcwd())
-    db_url = config['db_url']
-    repo = config['repo']
+def invoke_alembic():
+    galaxy_config = GalaxyAppConfiguration()
+    db_url = galaxy_config.database_connection
+    install_db_url = galaxy_config.install_database_connection
+    sys.argv.insert(1, f'tsi_url={install_db_url}')
+    sys.argv.insert(1, '-x')
+    sys.argv.insert(1, f'gxy_url={db_url}')
+    sys.argv.insert(1, '-x')
+    alembic.config.main()
 
-    main(repository=repo, url=db_url)
 
-
-if __name__ == "__main__":
-    invoke_migrate_main()
+if __name__ == '__main__':
+    invoke_alembic()
