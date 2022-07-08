165dc0b34adbfb9b80ec01968a4ecd68fd0cc2b3
John Davis <jdavcs@gmail.com>
2022-07-08 16:05:50 -0400

    [TUTORIAL] Add bytesize fixture

--- test/unit/util/test_bytesize.py
+++ test/unit/util/test_bytesize.py
@@ -25,35 +25,37 @@ def test_parse_bytesize_str_invalid():
         parse_bytesize('invalid-value')
 
 
-def test_bytesize_to_unit():
-    bs = ByteSize(1_000_000_000_000_000)
-    assert bs.to_unit('k') == '1000000000000K'
-    assert bs.to_unit('m') == '1000000000M'
-    assert bs.to_unit('g') == '1000000G'
-    assert bs.to_unit('t') == '1000T'
-    assert bs.to_unit('p') == '1P'
-    assert bs.to_unit('e') == '0E'
-
-    assert bs.to_unit('ki') == '976562500000KI'
-    assert bs.to_unit('mi') == '953674316MI'
-    assert bs.to_unit('gi') == '931322GI'
-    assert bs.to_unit('ti') == '909TI'
-    assert bs.to_unit('pi') == '0PI'
-    assert bs.to_unit('ei') == '0EI'
-
-    assert bs.to_unit() == '1000000000000000'  # no unit
-    assert bs.to_unit('K') == '1000000000000K'  # uppercase unit
+@pytest.fixture
+def bytesize():
+    return ByteSize(1_000_000_000_000_000)
 
 
-def test_bytesize_to_unit_as_str():
-    bs = ByteSize(1_000_000_000_000_000)
-    assert bs.to_unit('k', as_string=False) == 1000000000000  # not to_string
+def test_bytesize_to_unit(bytesize):
+    assert bytesize.to_unit('k') == '1000000000000K'
+    assert bytesize.to_unit('m') == '1000000000M'
+    assert bytesize.to_unit('g') == '1000000G'
+    assert bytesize.to_unit('t') == '1000T'
+    assert bytesize.to_unit('p') == '1P'
+    assert bytesize.to_unit('e') == '0E'
+
+    assert bytesize.to_unit('ki') == '976562500000KI'
+    assert bytesize.to_unit('mi') == '953674316MI'
+    assert bytesize.to_unit('gi') == '931322GI'
+    assert bytesize.to_unit('ti') == '909TI'
+    assert bytesize.to_unit('pi') == '0PI'
+    assert bytesize.to_unit('ei') == '0EI'
+
+    assert bytesize.to_unit() == '1000000000000000'  # no unit
+    assert bytesize.to_unit('K') == '1000000000000K'  # uppercase unit
 
 
-def test_bytesize_to_unit_invalid():
-    bs = ByteSize(1_000_000_000_000_000)
+def test_bytesize_to_unit_as_str(bytesize):
+    assert bytesize.to_unit('k', as_string=False) == 1000000000000  # not to_string
+
+
+def test_bytesize_to_unit_invalid(bytesize):
     with pytest.raises(KeyError):
-        bs.to_unit('invalid')
+        bytesize.to_unit('invalid')
 
 
 @pytest.mark.parametrize(
@@ -75,6 +77,5 @@ def test_bytesize_to_unit_invalid():
         ('K', '1000000000000K'),
     ]
 )
-def test_bytesize_to_unit_better(unit, expected_value):
-    bs = ByteSize(1_000_000_000_000_000)
-    assert bs.to_unit(unit) == expected_value
+def test_bytesize_to_unit_better(unit, expected_value, bytesize):
+    assert bytesize.to_unit(unit) == expected_value
