4c57067a0c8be6a9848f87a02a99105ec76de780
John Davis <jdavcs@gmail.com>
2022-07-10 15:26:03 -0400

    [api] populator use

--- lib/galaxy_test/api/test_mytutorial.py
+++ lib/galaxy_test/api/test_mytutorial.py
@@ -51,34 +51,50 @@ class MyTutorialApiTestCase(ApiTestCase):
         super().setUp()
         self.dataset_populator = DatasetPopulator(self.galaxy_interactor)
 
-    def test_create_role(self):
-        
-        # create role
-        name = self.dataset_populator.get_random_name()
-        description = 'description of this cool role'
-        payload = {
-            "name": name,
-            "description": description,
-        }
+#    def test_create_role(self):
+#        
+#        # create role
+#        name = self.dataset_populator.get_random_name()
+#        description = 'description of this cool role'
+#        payload = {
+#            "name": name,
+#            "description": description,
+#        }
+#
+#        # verify role not in roles
+#        response = self._get("roles")
+#        response.raise_for_status()
+#        data = response.json()
+#        assert not any(role["name"] == name for role in data)
+#
+#        # TODO do i need these params???
+#        response = self._post("roles", payload, admin=True, json=True)
+#        response.raise_for_status()
+#        role = response.json()
+#        assert role["name"] == name
+#        assert role["description"] == description
+#
+#        # verify role has been added
+#        response = self._get("roles")
+#        response.raise_for_status()
+#        data = response.json()
+#        assert any(role["name"] == name for role in data)
 
-        # verify role not in roles
-        response = self._get("roles")
-        response.raise_for_status()
-        data = response.json()
-        assert not any(role["name"] == name for role in data)
 
-        # TODO do i need these params???
-        response = self._post("roles", payload, admin=True, json=True)
-        response.raise_for_status()
-        role = response.json()
-        assert role["name"] == name
-        assert role["description"] == description
+    def test_search_dataset_by_history(self):
+        self.history1_id = self.dataset_populator.new_history()
+        self.history2_id = self.dataset_populator.new_history()
 
-        # verify role has been added
-        response = self._get("roles")
-        response.raise_for_status()
-        data = response.json()
-        assert any(role["name"] == name for role in data)
+        history1_datasets, history2_datasets = 2, 3
+        for _ in range(history1_datasets):
+            self.dataset_populator.new_dataset(self.history1_id)
+        for _ in range(history2_datasets):
+            self.dataset_populator.new_dataset(self.history2_id)
 
+        payload = { "history_id": self.history1_id, }
+        response = self._get("datasets", payload).json()
+        assert len(response) == history1_datasets
 
-
+        payload = { "history_id": self.history2_id, }
+        response = self._get("datasets", payload).json()
+        assert len(response) == history2_datasets
