364273190ba68807df2e6c857025febe2c5eb791
John Davis <jdavcs@gmail.com>
2023-08-08 00:39:24 -0400

    use sa20 syntax

--- lib/galaxy/managers/users.py
+++ lib/galaxy/managers/users.py
@@ -20,6 +20,7 @@ from sqlalchemy import (
     and_,
     exc,
     func,
+    select,
     true,
 )
 from sqlalchemy.orm.exc import NoResultFound
@@ -843,7 +844,8 @@ def get_user_by_username(app, username):
     """Get a user from the database by username."""
     sa_session = app.model.session
     try:
-        user = sa_session.query(app.model.User).filter(app.model.User.table.c.username == username).one()
+        stmt = select(model.User).filter(model.User.username == username)
+        user = session.execute(stmt).scalar_one()
         return user
     except Exception:
         return None
