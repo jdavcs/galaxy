91406e716c58b3a9045334d4fccfd7d18ee55bdc
John Davis <jdavcs@gmail.com>
2023-08-17 13:51:41 -0400

    Add hda_repo.get_fasta_hdas_by_history

--- lib/galaxy/model/repositories/hda.py
+++ lib/galaxy/model/repositories/hda.py
@@ -1,3 +1,5 @@
+from sqlalchemy import select
+
 from galaxy.model import HistoryDatasetAssociation
 from galaxy.model.repositories import (
     ModelRepository,
@@ -9,3 +11,12 @@ from galaxy.model.repositories import (
 class HistoryDatasetAssociationRepository(ModelRepository):
     def __init__(self, session: SessionType):
         super().__init__(session, HistoryDatasetAssociation)  # type:ignore[arg-type]
+
+    def get_fasta_hdas_by_history(self, history_id: int):
+        # type-ignore/SessionlessContext
+        stmt = (
+            select(HistoryDatasetAssociation)
+            .filter_by(history_id=history_id, extension="fasta", deleted=False)
+            .order_by(HistoryDatasetAssociation.hid.desc())  # type:ignore[attr-defined]
+        )
+        return self.session.scalars(stmt).all()  # type:ignore[union-attr]
