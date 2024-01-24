11b96bb2be6a73a2b1e3372f634a2fe3fdcbeda1
John Davis <jdavcs@gmail.com>
2024-01-24 16:16:40 -0500

    Add typing to user attr in UserShareAssoc models

--- lib/galaxy/model/__init__.py
+++ lib/galaxy/model/__init__.py
@@ -3549,7 +3549,7 @@ class History(Base, HasTags, Dictifiable, UsesAnnotations, HasName, Serializable
 
 
 class UserShareAssociation(RepresentById):
-    user: Optional[User]
+    user: Mapped[User]
 
 
 class HistoryUserShareAssociation(Base, UserShareAssociation):
@@ -3558,7 +3558,7 @@ class HistoryUserShareAssociation(Base, UserShareAssociation):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     history_id: Mapped[Optional[int]] = mapped_column(Integer, ForeignKey("history.id"), index=True)
     user_id: Mapped[Optional[int]] = mapped_column(Integer, ForeignKey("galaxy_user.id"), index=True)
-    user = relationship("User")
+    user: Mapped[User] = relationship("User")
     history = relationship("History", back_populates="users_shared_with")
 
 
@@ -8307,7 +8307,7 @@ class StoredWorkflowUserShareAssociation(Base, UserShareAssociation):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     stored_workflow_id: Mapped[Optional[int]] = mapped_column(Integer, ForeignKey("stored_workflow.id"), index=True)
     user_id: Mapped[Optional[int]] = mapped_column(Integer, ForeignKey("galaxy_user.id"), index=True)
-    user = relationship("User")
+    user: Mapped[User] = relationship("User")
     stored_workflow = relationship("StoredWorkflow", back_populates="users_shared_with")
 
 
@@ -10140,7 +10140,7 @@ class PageUserShareAssociation(Base, UserShareAssociation):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     page_id: Mapped[Optional[int]] = mapped_column(Integer, ForeignKey("page.id"), index=True)
     user_id: Mapped[Optional[int]] = mapped_column(Integer, ForeignKey("galaxy_user.id"), index=True)
-    user = relationship("User")
+    user: Mapped[User] = relationship("User")
     page = relationship("Page", back_populates="users_shared_with")
 
 
@@ -10296,7 +10296,7 @@ class VisualizationUserShareAssociation(Base, UserShareAssociation):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     visualization_id: Mapped[Optional[int]] = mapped_column(Integer, ForeignKey("visualization.id"), index=True)
     user_id: Mapped[Optional[int]] = mapped_column(Integer, ForeignKey("galaxy_user.id"), index=True)
-    user = relationship("User")
+    user: Mapped[User] = relationship("User")
     visualization = relationship("Visualization", back_populates="users_shared_with")
 
 
