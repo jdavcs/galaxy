85b68eb19a002e21b6c1381e52c30140a3effbb7
John Davis <jdavcs@gmail.com>
2024-01-24 15:48:13 -0500

    Fix mypy error: return 0 if no results

--- lib/galaxy/model/__init__.py
+++ lib/galaxy/model/__init__.py
@@ -1198,7 +1198,7 @@ ON CONFLICT
         stmt = select(func.count()).select_from(stmt)
         session = object_session(self)
         assert session
-        return session.scalar(stmt)
+        return session.scalar(stmt) or 0
 
 
 class PasswordResetToken(Base):
