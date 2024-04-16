a3c2763eaa26da0e734285e85305ea3105eb5b8f
John Davis <jdavcs@gmail.com>
2024-04-16 15:21:03 -0400

    Move+test get_users_by_ids

--- test/unit/data/db/test_user.py
+++ test/unit/data/db/test_user.py
@@ -1,7 +1,9 @@
 from galaxy.model.db.user import (
     get_user_by_email,
     get_user_by_username,
+    get_users_by_ids,
 )
+from . import verify_items
 
 
 def test_get_user_by_username(session, make_user, make_random_users):
@@ -18,3 +20,13 @@ def test_get_user_by_email(session, make_user, make_random_users):
 
     user = get_user_by_email(session, "a@foo.bar")
     assert user is my_user
+
+
+def test_get_users_by_ids(session, make_random_users):
+    users = make_random_users(10)
+    u1, u2, u3 = users[0], users[3], users[7]  # select some random users
+    ids = [u1.id, u2.id, u3.id]
+
+    users2 = get_users_by_ids(session, ids)
+    expected = [u1, u2, u3]
+    verify_items(users2, expected)
